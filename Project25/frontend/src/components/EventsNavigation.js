b9ccf1a847f52d4222209e9266e807aa1d18d2f3
Wawrzynn <jedrzejkosakowski@gmail.com>
2023-08-14 15:41:26 +0200

    Project 25 in progress.

---
+++ Project25/frontend/src/components/EventsNavigation.js
@@ -0,0 +1,36 @@
+import { NavLink } from "react-router-dom";
+import classes from "./EventsNavigation.module.css";
+
+function EventsNavigation() {
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul className={classes.list}>
+          <li>
+            <NavLink
+              to="/events"
+              className={({ isActive }) =>
+                isActive ? classes.active : classes.inactive
+              }
+              end
+            >
+              All Events
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/events/new"
+              lassName={({ isActive }) =>
+                isActive ? classes.active : classes.inactive
+              }
+            >
+              New Event
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default EventsNavigation;
