32ae7886c61a2a402fdea2523042d3ae1ff9a639
Wawrzynn <jedrzejkosakowski@gmail.com>
2023-09-01 13:14:06 +0200

    Another lesson. Added form to NewEvent.

--- Project25/frontend/src/pages/NewEventPage.js
+++ Project25/frontend/src/pages/NewEventPage.js
@@ -1,7 +1,33 @@
 import EventForm from "../components/EventForm";
+import { json, redirect } from "react-router-dom";
 
 function NewEventPage() {
-    return <EventForm />;
+  return <EventForm />;
 }
 
 export default NewEventPage;
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    return json({ message: "Could not create event" }, { status: 500 });
+  }
+
+  return redirect(`/events`);
+}
